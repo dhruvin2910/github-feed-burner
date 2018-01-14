5b3cb7dae6808e3ed3cc14580d86878d59bdba0f
Dhruvin Gandhi <dhruvin2910@gmail.com>
2018-01-14 10:24:31 +0530

    Initialize a vue app

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hello-world">
+    {{message}}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return {
+        message: 'Hello World'
+      }
+    }
+  }
+</script>
